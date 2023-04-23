03b540d503722c9dfb966af63a613b733e624e76
Santosh <santoshkumar.kollati@gmail.com>
2023-04-23 03:51:28 -0600

    added mongo.repo file and mongo.sh, made the repo file to be included in mongodb.sh to be copied to it's dir

---
+++ mongodb.sh
@@ -0,0 +1,7 @@
+set-hostname mongodb
+cd mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+# mongo.conf needs to be edited to Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
