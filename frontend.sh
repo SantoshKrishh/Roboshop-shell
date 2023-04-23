338ed1acb0be2340780908b5d7e6ad36517e6617
Santosh <santoshkumar.kollati@gmail.com>
2023-04-23 03:23:27 -0600

    created roboshop.conf file and added it to frontend.sh script for copy paste action

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,11 @@
 set-hostname frontend
 yum install nginx -y
+# roboshop.conf file is created separately in git repo and is made to be copied to desired dir in the instance
+
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
-# roboshop.conf file needs to be created
-
  systemctl enable nginx
  systemctl start nginx
