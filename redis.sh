b3a9e8d5b9a29b69c334f3356349b7ffc3c0bf58
Santosh <santoshkumar.kollati@gmail.com>
2023-04-24 11:44:11 -0600

    added print messages and added sed command to update listen address

--- redis.sh
+++ redis.sh
@@ -1,6 +1,16 @@
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Modifying Redis repo<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Enable Redis 6.2<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Install Redis<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install redis -y
-echo "Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf"
+
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Updating listen address<<<<<<<<<<<<<<<<<<<\e[0m"
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf  -e "s/127.0.0.1/0.0.0.0/" /etc/redis/redis.conf
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Restarting systemD<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
