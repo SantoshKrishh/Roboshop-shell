0e0aa321e0e8ccfc2c3d8184978b9e214c4b5fc8
Santosh <santoshkumar.kollati@gmail.com>
2023-04-24 10:23:27 -0600

    added print messages and changed copy pat

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,20 @@
-
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Install Nginx<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install nginx -y
 # roboshop.conf file is created separately in git repo and is made to be copied to desired dir in the instance
 
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Copying Roboshop Configuration file <<<<<<<<<<<<<<<<<<<\e[0m"
+cp /home/centos/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Removing web server default content<<<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Extracting frontend content.<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Restarting Nginx Service<<<<<<<<<<<<<<<<<<<\e[0m"
  systemctl enable nginx
- systemctl start nginx
+ systemctl restart nginx
 
  echo "Ensure you replace the localhost with the actual ip address of those component server. Word localhost is just used to avoid the failures on the Nginx Server."
