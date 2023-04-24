334e43327c68a8ea57a560128f7741f192596d76
Santosh <santoshkumar.kollati@gmail.com>
2023-04-24 07:59:54 -0600

    added sed command to replace 127.0.0.1 to 0.0.0.0

--- mongodb.sh
+++ mongodb.sh
@@ -4,5 +4,5 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
-echo "mongo.conf needs to be edited to Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf"
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
 echo "You need to update catalogue server ip address in frontend configuration. Configuration file is /etc/nginx/default.d/roboshop.conf"
