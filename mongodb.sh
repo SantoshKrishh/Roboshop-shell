4c93d9772fbcdcbebc218fc600166b98d882dcdf
Santosh <santoshkumar.kollati@gmail.com>
2023-04-24 11:36:08 -0600

    added print messages and changed copy path for mongo repo

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,16 @@
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Copying Mongo Repo file<<<<<<<<<<<<<<<<<<<\e[0m"
+cp /home/centos/Roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Installing MongoDB<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org -y
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Updating listen address<<<<<<<<<<<<<<<<<<<\e[0m"
 sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+
+echo -e "\e[34m>>>>>>>>>>>>>>>>>Restart the service<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 
 echo "You need to update catalogue server ip address in frontend configuration. Configuration file is /etc/nginx/default.d/roboshop.conf"
+
