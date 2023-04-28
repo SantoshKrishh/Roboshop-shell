2b23b60c6c2c2da33d337e607dbfdabd0518a55e
Santosh <santoshkumar.kollati@gmail.com>
2023-04-28 10:28:46 -0600

    added if then condition for conditional arguments pass check

--- payment.sh
+++ payment.sh
@@ -3,6 +3,11 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_user_password=$1
 
+if [ -z "$rabbitmq_user_password" ]; then
+   echo input rabbitmq user password is missing
+   exit
+fi
+
 echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
 
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,6 +3,11 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_appuser_password=$1
 
+if [ -z "$rabbitmq_appuser_password" ]; then
+   echo input rabbitmq appuser password is missing
+   exit
+fi
+
 echo -e "\e[36m>>>>>>>>> Setup ErLang Repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
